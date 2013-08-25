a554a2e9c07bc38ab13a7ffca05300bd1920cceb
Maxim Kornilov <maxim.kornilov@beaver-studio.com>
2013-08-25 21:41:37 +0400

    Committed initial sources for VS project which creates NuGet package which contains target to copy linked content files.
    Added customized VS project which has NuGet package as build output.

---
+++ Source/tools/install.ps1
@@ -0,0 +1,11 @@
+﻿# Runs every time a package is installed in a project
+
+param($installPath, $toolsPath, $package, $project)
+
+# $installPath is the path to the folder where the package is installed.
+# $toolsPath is the path to the tools directory in the folder where the package is installed.
+# $package is a reference to the package object.
+# $project is a reference to the project the package was installed to.
+
+# Removes dummy content text file which is used only to make this nuget package to work on project scope level.
+$project.ProjectItems | where {$_.Name -eq "MSBuild.WebApplication.CopyContentLinkedFiles.Dummy.txt"} | ForEach-Object { $_.Remove() }
